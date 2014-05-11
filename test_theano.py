070570bb1cc9495c5069dbb5eff9c7e78ca15335
Andrew Clegg <andrew.clegg@pearson.com>
2014-05-11 17:10:41 +0100

    Logging of retries in theano_test.py

--- test_theano.py
+++ test_theano.py
@@ -6,7 +6,12 @@ import theano
 theano.config.compute_test_value = 'off'
 result = theano.test(extra_argv=['-x'])
 if not result.wasSuccessful():
-    # Try again with appropriate param reset
+    print()
+    print('RETRYING WITH TEST VALUES DISABLED')
+    print('**********************************')
+    print()
+    print('Please ignore any error messages above here. Any below here *do* apply, however.')
+    print()
     theano.config.compute_test_value = 'off'
     theano.test()
 
