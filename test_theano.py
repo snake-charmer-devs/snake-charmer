b5d5eb64ad21bb7e3db0ed094b42d9fd08718d80
Andrew Clegg <andrew.clegg@pearson.com>
2014-05-11 22:15:53 +0100

    More test threading jiggery pokery

--- test_theano.py
+++ test_theano.py
@@ -2,12 +2,15 @@
 # to 'raise', then fails to set it back, causing lots of other tests to fail.
 # But this only happens the first time... So you can catch it and retry.
 
+import math
 import multiprocessing
-cpus = multiprocessing.cpu_count()
+threads = math.ceil(multiprocessing.cpu_count() / 2)
+threads_arg = '--processes=%d' % (threads)
+time_arg = '--process-timeout=9999'
 
 import theano
 theano.config.compute_test_value = 'off'
-result = theano.test(extra_argv=['-x', '--processes=%d' % (cpus)])
+result = theano.test(extra_argv=['-x', threads_arg, time_arg])
 if not result.wasSuccessful():
     print()
     print('RETRYING WITH TEST VALUES DISABLED')
@@ -16,5 +19,5 @@ if not result.wasSuccessful():
     print('Please ignore any error messages above here. Any below here *do* apply, however.')
     print()
     theano.config.compute_test_value = 'off'
-    theano.test(extra_argv=['--processes=%d' % (cpus)])
+    theano.test(extra_argv=[threads_arg, time_arg])
 
